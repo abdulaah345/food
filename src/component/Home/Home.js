ef56575f30f5ea0c86ae12e097d00fa3e2ca4379
abdullah345 <abdallahmohamed54655@gmail.com>
2024-06-13 14:52:15 +0300

    .

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from './Header'
 import './Home.css';
 import pic from './../../assets/sec.JPG'
+import integ from './../../assets/int.JPG'
+
 const Home = () => {
   return (
     <>
@@ -45,6 +47,43 @@ const Home = () => {
             </div>
 
           </div>
+
+        </section>
+        <section className='integ'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-md-6'>
+              <h2>We make everything by hand with the best possible ingredients.</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio finibus bibendum in sit amet leo. Mauris feugiat erat tellus.Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                 <ul>
+                  <li>Etiam sed dolor ac diam volutpat.</li>
+                  <li>Erat volutpat aliquet imperdiet. </li>
+                  <li>purus a odio finibus bibendum.</li>
+               
+                 </ul>
+                  <button><a href='#'>Learn More</a></button>
+                
+              </div>
+              <div className='col-md-6'>
+              <img src={integ} alt='integ'></img>
+              </div>
+
+            </div>
+
+          </div>
+
+        </section>
+        <section className='pradiex'>
+          <div className='container'>
+            <div className='row'>
+              <div className='co-lg-12 co-md-12'>
+                  <h2>When a man's stomach is full it makes no <br/> difference whether he is rich or poor.</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio     <br/>             finibus bibendum in sit amet leo. Mauris feugiat erat tellus.</p>
+                  <a href='#'>Watch Our Story</a>
+              </div>
+
+            </div>
+            </div> 
 
         </section>
     </>
